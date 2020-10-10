7cff77205a315cac66cd91d737db78b5b1d4139b
vaida898 <freelpfm@gmail.com>
2020-10-10 21:07:25 +0000

    added comments to js file to explain what is happening in a code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// API Key and URL address
 const api = {
     key: "c7083b6846aa8a85455185faa6de23ba",
     baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
+// city serach 
 const citySearch = document.querySelector('.search');
 citySearch.addEventListener('keypress', findCity);
 
@@ -12,6 +14,7 @@ function findCity(e) {
     }
 }
 
+// fetching data from API
 function searchResults (query) {
     fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
@@ -19,11 +22,13 @@ function searchResults (query) {
         }).then(displayResults);
 }
 
+// displaying data for end user on. screen
 function displayResults (weather) {
     console.log(weather);
     let currentCity = document.querySelector('.current-location .city');
     currentCity.innerText = `${weather.name}`;
 
+    // display current date
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
 
@@ -32,16 +37,19 @@ function displayResults (weather) {
 
     document.querySelector('.current-location .date').innerText = date;
 
+    // display current temperature
     let temperature = document.querySelector('.current-temp .temperature');
     temperature.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
 
+    // display current weather condition
     let weatherDescription = document.querySelector('.current-temp .weather-cond');
     weatherDescription.innerText = weather.weather[0].main;
 
+    // display weather icon which describbes weather condition
     let showWeatherIcon = document.querySelector('.current-temp .weather-icon');
     showWeatherIcon.innerHTML = `<img src="assets/img/icons/${weather.weather[0].icon}.png"/>`;
 
-
+    // show high and low temperature measures
     let hiLowTemp = document.querySelector('.current-temp .temp-diff');
     hiLowTemp.innerHTML = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
 }
