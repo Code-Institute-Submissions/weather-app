b570bc1a9d763cdf0e691ee3cf24f0c98dc2d433
vaida898 <freelpfm@gmail.com>
2020-10-14 22:05:11 +0000

    added submit button with kesubmit button and keypress to trigger findCity() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const temperatureRef = document.querySelector('.current-temp .temperature');
 const weatherDescriptionRef = document.querySelector('.current-temp .weather-cond');
 const showWeatherIconRef = document.querySelector('.current-temp .weather-icon');
 const currentLocationDate = document.querySelector('.current-location .date');
+const submitBtn = document.getElementById('submit-btn');
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
 'July', 'August', 'September', 'October', 'November', 'December'];
 const api = {
@@ -15,39 +16,49 @@ const api = {
  * Searching for a city
  * @param e {string} - Id of the city to provide for API
  */
-function findCity(e) {
-  if (e.keyCode == 13) {
+function findCity() {
     searchResults(citySearch.value);
-  }
+    document.getElementById('submit-btn').click;
 }
-citySearch.addEventListener('keypress', findCity);
+window.addEventListener('keypress', function(event){
+    if (event.keyCode == 13) {
+        findCity();
+    }
+});
 /**
  * Searching the weather api and will run displayResults
  * @param query {string} - The name of the city
  */
 function searchResults(query) {
   fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults);
+  .then((response) => response.json())
+  .then((weatherData) => displayResults(weatherData))
+  .catch((err) => {
+      $('#error').css('display', 'block');
+      $('current-location').css('display', 'none');
+  });
 }
+   // .then((weather) => {
+    //  return weather.json();
+   // })
+   // .then(displayResults);
+//}
 /**
  * Displays the api results to the user
  * @param weather {object} - Weather Object coming from APi
  */
-function displayResults(weather) {
-  currentCity.innerText = `${weather.name}`;
+function displayResults(weatherData) {
+  currentCity.innerText = `${weatherData.name}`;
   const currentDate = new Date();
   const date = currentDate.getDate() + ' ' + monthNames[currentDate.getMonth()] + ' ' + currentDate.getFullYear();
   currentLocationDate.innerText = date;
-  if (weather.coord.dt > weather.sys.sunrise && weather.coord.dt < weather.sys.sunset) {
+  if (weatherData.coord.dt > weatherData.sys.sunrise && weatherData.coord.dt < weatherData.sys.sunset) {
     $('#background').css('background', 'url(assets/img/PM.jpg)');
   } else {
     $('#background').css('background', 'url(assets/img/AM.jpg)');
   }
-  temperatureRef.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
-  weatherDescriptionRef.innerText = weather.weather[0].main;
-  showWeatherIconRef.innerHTML = `<img src="assets/img/icons/${weather.weather[0].icon}.png">`;
-  hiLowTempRef.innerHTML = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+  temperatureRef.innerHTML = `${Math.round(weatherData.main.temp)}<span>°C</span>`;
+  weatherDescriptionRef.innerText = weatherData.weather[0].main;
+  showWeatherIconRef.innerHTML = `<img src="assets/img/icons/${weatherData.weather[0].icon}.png">`;
+  hiLowTempRef.innerHTML = `${Math.round(weatherData.main.temp_min)}°C / ${Math.round(weatherData.main.temp_max)}°C`;
 }
